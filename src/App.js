74f095dee44f5196ea258e1d1acf8dbeee5a649f
Andreas Olsson <andreasolsson660@gmail.com>
2020-05-28 19:43:51 +0200

    added routes to pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './App.css';
 import { Button } from 'reactstrap';
 import Navigation from './components/Navigation'
+import {Switch, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Cart from "./pages/Cart";
+
 function App() {
   return (
     <div>
       <Navigation />
-      <p>hello</p>
-      <Button outline color="primary">primary</Button>{' '}
+
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Cart />
+          </Route>
+        </Switch>
+      </div>
     </div>
+    // End of container
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,33 +1,42 @@
 import React, { useState } from 'react'
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import {Link} from 'react-router-dom'
 
 export default function Navigation(props) {
     const [collapsed, setCollapsed] = useState(true);
 
     const toggleNavbar = () => setCollapsed(!collapsed);
     return (
-        <div>
-            <Navbar color="faded" light expand="md">
-                <div className="container">
-                <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
-                <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-                <Collapse isOpen={!collapsed} navbar>
-                    <Nav navbar className="mr-auto">
-                        <NavItem>
-                            <NavLink href="/components/">Components</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                        </NavItem>
-                    </Nav>
-                    <Nav navbar>
-                        <NavItem>
-                                <NavLink href="https://github.com/reactstrap/reactstrap"><i class="ri-shopping-cart-fill"></i></NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-                </div>
-            </Navbar>
-        </div>
-    )
+      <div>
+        <Navbar color="faded" light expand="md">
+          <div className="container">
+            <NavbarBrand className="mr-auto">
+              <Link to="/">Shop name</Link>
+            </NavbarBrand>
+            <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+            <Collapse isOpen={!collapsed} navbar>
+              <Nav navbar className="mr-auto">
+                <NavItem>
+                  <NavLink href="/components/">Components</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap">
+                    GitHub
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap">
+                    <Link to="/home">
+                      <i class="ri-shopping-cart-fill"></i>
+                    </Link>
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+      </div>
+    );
 }

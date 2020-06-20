72439e62e59544e3ceb4e5665492273c6e5d2422
Andreas Olsson <andreasolsson660@gmail.com>
2020-06-20 13:59:54 +0200

    changed products

--- src/products.js
+++ src/products.js
@@ -2,25 +2,24 @@ let products = [
   {
     Title: "Awesome product",
     Price: 3.98,
-    Img: "/img/couch.jpg",
+    Img: "/img/sneakers_1.jpg",
     Id: "1",
     isFavorite: false,
   },
   {
     Title: "Another Awesome product",
     Price: 3.98,
-    Img: "/img/couch.jpg",
+    Img: "/img/sneakers_2.jpg",
     Id: "2",
     isFavorite: false,
   },
   {
     Title: "A miracle product",
     Price: 3.98,
-    Img: "/img/couch.jpg",
+    Img: "/img/sneakers_3.jpg",
     Id: "3",
     isFavorite: false,
   },
- 
 ];
 
 export default products

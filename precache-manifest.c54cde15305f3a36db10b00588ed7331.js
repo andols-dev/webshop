52289b9886372abfb9419fa9e6f8af292afcaaa3
Andreas Olsson <andreasolsson660@gmail.com>
2020-06-20 14:18:27 +0200

    Updates

---
+++ precache-manifest.c54cde15305f3a36db10b00588ed7331.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec4d519b6f240bc7e659f50a124265e",
+    "url": "/webshop/index.html"
+  },
+  {
+    "revision": "72732e7d7320dcb4578c",
+    "url": "/webshop/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "31aed4737fb810fe37a3",
+    "url": "/webshop/static/css/main.94800db1.chunk.css"
+  },
+  {
+    "revision": "72732e7d7320dcb4578c",
+    "url": "/webshop/static/js/2.7e01c5ff.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/webshop/static/js/2.7e01c5ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31aed4737fb810fe37a3",
+    "url": "/webshop/static/js/main.1d466f85.chunk.js"
+  },
+  {
+    "revision": "310ff645f9327b8c51b1",
+    "url": "/webshop/static/js/runtime-main.daa844bb.js"
+  }
+]);
